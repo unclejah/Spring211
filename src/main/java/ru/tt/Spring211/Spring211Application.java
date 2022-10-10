82784cb4bedab390691295d2990995f6a5e8fa22
jah <celt8@yandex.ru>
2022-10-10 11:42:55 +0500

    -init commit

---
+++ src/main/java/ru/tt/Spring211/Spring211Application.java
@@ -0,0 +1,13 @@
+package ru.tt.Spring211;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring211Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring211Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/tt/Spring211/Spring211ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tt.Spring211;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring211ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
